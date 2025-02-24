f8986af26e4f9717ddc42c27636075d2cc3e719e
taobitk <8hungvip8@gmail.com>
2025-02-24 18:40:39 +0700

    case

--- sql/casestudy sql 2.sql
+++ sql/casestudy sql 2.sql
@@ -8,7 +8,8 @@ select * from nhan_vien
 where ho_ten like 'H%' 
 or ho_ten like 'T%' 
 or ho_ten like 'K%' 
-AND LENGTH(ho_ten) <= 15; 
+-- AND length(ho_ten) <= 15; 
+AND char_length(ho_ten) <= 15; 
 
 
 -- 3.	Hiển thị thông tin của tất cả khách hàng có độ tuổi từ 18 đến 50 tuổi và có địa chỉ ở “Đà Nẵng” hoặc “Quảng Trị”.
@@ -20,12 +21,11 @@ AND (dia_chi LIKE '%Đà Nẵng%' OR dia_chi LIKE '%Quảng Trị%');
 -- 4.	Đếm xem tương ứng với mỗi khách hàng đã từng đặt phòng bao nhiêu lần. 
 --      Kết quả hiển thị được sắp xếp tăng dần theo số lần đặt phòng của khách hàng. 
 --      Chỉ đếm những khách hàng nào có Tên loại khách hàng là “Diamond”.
-
+select * from khach_hang;
 SELECT kh.ho_ten, lk.ten_loai_khach, COUNT(hd.ma_hop_dong) AS so_lan_dat_phong
 FROM khach_hang kh
 JOIN loai_khach lk ON kh.ma_loai_khach = lk.ma_loai_khach
-JOIN hop_dong hd ON kh.ma_khach_hang = hd.ma_khach_hang
-WHERE lk.ten_loai_khach = 'Diamond'
+left JOIN hop_dong hd ON kh.ma_khach_hang = hd.ma_khach_hang
 GROUP BY kh.ma_khach_hang
 ORDER BY so_lan_dat_phong ASC;
 
@@ -33,51 +33,45 @@ ORDER BY so_lan_dat_phong ASC;
 -- 5.	Hiển thị ma_khach_hang, ho_ten, ten_loai_khach, ma_hop_dong, ten_dich_vu, ngay_lam_hop_dong, ngay_ket_thuc, tong_tien 
 -- (Với tổng tiền được tính theo công thức như sau: Chi Phí Thuê + Số Lượng * Giá, với Số Lượng và Giá là từ bảng dich_vu_di_kem, hop_dong_chi_tiet) 
 -- cho tất cả các khách hàng đã từng đặt phòng. (những khách hàng nào chưa từng đặt phòng cũng phải hiển thị ra).
+
 SELECT 
-    kh.ma_khach_hang, 
-    kh.ho_ten, 
+    k.ma_khach_hang, 
+    k.ho_ten, 
     lk.ten_loai_khach, 
     hd.ma_hop_dong, 
     dv.ten_dich_vu, 
     hd.ngay_lam_hop_dong, 
-    hd.ngay_ket_thuc, 
-    (dv.chi_phi_thue + IFNULL(dvdk.gia * 1, 0)) AS tong_tien
-FROM 
-    khach_hang kh
-JOIN 
-    loai_khach lk ON kh.ma_loai_khach = lk.ma_loai_khach
-LEFT JOIN 
-    hop_dong hd ON kh.ma_khach_hang = hd.ma_khach_hang
-LEFT JOIN 
-    dich_vu dv ON hd.ma_dich_vu = dv.ma_dich_vu
-LEFT JOIN 
-    dich_vu_di_kem dvdk ON hd.ma_hop_dong = dvdk.ma_dich_vu_di_kem
-ORDER BY 
-    kh.ma_khach_hang, hd.ma_hop_dong;
+    hd.ngay_ket_thuc,
+    SUM(IFNULL(hdct.so_luong * dvdk.gia, 0)) + SUM(IFNULL(dv.chi_phi_thue, 0)) AS tong_tien
+FROM khach_hang k
+LEFT JOIN loai_khach lk ON k.ma_loai_khach = lk.ma_loai_khach
+LEFT JOIN hop_dong hd ON k.ma_khach_hang = hd.ma_khach_hang
+LEFT JOIN hop_dong_chi_tiet hdct ON hd.ma_hop_dong = hdct.ma_hop_dong
+LEFT JOIN dich_vu_di_kem dvdk ON hdct.ma_dich_vu_di_kem = dvdk.ma_dich_vu_di_kem
+LEFT JOIN dich_vu dv ON hd.ma_dich_vu = dv.ma_dich_vu
+GROUP BY 
+    k.ma_khach_hang, 
+    lk.ten_loai_khach, 
+    hd.ma_hop_dong, 
+    dv.ten_dich_vu, 
+    hd.ngay_lam_hop_dong, 
+    hd.ngay_ket_thuc
+ORDER BY k.ma_khach_hang;
 
 
 -- 6.	Hiển thị ma_dich_vu, ten_dich_vu, dien_tich, chi_phi_thue, ten_loai_dich_vu
 --  của tất cả các loại dịch vụ chưa từng được khách hàng thực hiện đặt từ quý 1 của năm 2021 (Quý 1 là tháng 1, 2, 3). 
--- để tạm đó 
-SELECT 
-    dv.ma_dich_vu, 
-    dv.ten_dich_vu, 
-    dv.dien_tich, 
-    dv.chi_phi_thue, 
-    ldv.ten_loai_dich_vu
-FROM 
-    dich_vu dv
-JOIN 
-    loai_dich_vu ldv ON dv.ma_loai_dich_vu = ldv.ma_loai_dich_vu
-JOIN 
-    hop_dong hd ON dv.ma_dich_vu = hd.ma_dich_vu
-WHERE 
-    (hd.ma_hop_dong IS NULL OR (YEAR(hd.ngay_lam_hop_dong) < 2021 OR MONTH(hd.ngay_lam_hop_dong) NOT IN (1, 2, 3)))
-group by 
-	ma_dich_vu
-ORDER BY 
-    dv.ma_dich_vu;
-    
+use furama;
+select dv.ma_dich_vu, dv.ten_dich_vu, dv.dien_tich, dv.chi_phi_thue, ldv.ten_loai_dich_vu
+from dich_vu dv
+join loai_dich_vu ldv on dv.ma_loai_dich_vu = ldv.ma_loai_dich_vu
+where dv.ma_dich_vu not in (
+    select distinct hd.ma_dich_vu
+    from hop_dong hd
+    where year(hd.ngay_lam_hop_dong) = 2021
+    and month(hd.ngay_lam_hop_dong) in (1, 2, 3)
+);
+
     
 -- 7.	Hiển thị thông tin ma_dich_vu, ten_dich_vu, dien_tich, so_nguoi_toi_da, chi_phi_thue, ten_loai_dich_vu
 -- của tất cả các loại dịch vụ đã từng được khách hàng đặt phòng trong năm 2020 nhưng chưa từng được khách hàng đặt phòng trong năm 2021.
